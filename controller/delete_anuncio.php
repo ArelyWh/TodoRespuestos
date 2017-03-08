83f4ceedded847cca720ef72ce833213eb1e442b
ArelyWh <whitearely21@gmail.com>
2017-03-08 14:26:58 -0600

    Upload folders

---
+++ controller/delete_anuncio.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'datacon.php';
+
+ if(isset($_REQUEST["Id"]) && !empty($_REQUEST["Id"]))
+ {
+    $id= (string) $_REQUEST['Id'];
+    $sql = "DELETE FROM productos WHERE IdProducto ='$id'";
+
+    if ($conn->query($sql) === TRUE) 
+    {
+        echo "Registro eliminado satisfactoriamente";
+    } 
+   // else 
+    //{
+     //   echo "Error: " . $sql . "<br>" . $conn->error;
+    //}
+ }
+
+$conn->close();
+
+?>
